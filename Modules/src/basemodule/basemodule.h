77090cf6ba2943c017c4dfcaf1996980b324aec2
Coopar998 <50998744+Coopar998@users.noreply.github.com>
2021-11-15 22:26:41 -0500

    Update basemodule.h

--- Modules/src/basemodule/basemodule.h
+++ Modules/src/basemodule/basemodule.h
@@ -3,13 +3,13 @@ namespace N
     class BaseModule
     {
     public:
-        int seed;
         bool checkSuccess();
         bool checkFailure();
         void runModule();
         void setupModule();
         char[] modID();
     protected:
+        int seed;
         bool successtriggered;
         bool failuretriggered;
         char[] modID = "base";
