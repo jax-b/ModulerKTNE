f0624d01ed4203112deba40454eda690fc29ebb1
Jackson Borneman <me@jaxb.net>
2021-11-17 17:12:38 -0500

    fixed wrong referance to class

--- Controler/SideControl/SideControl.cpp
+++ Controler/SideControl/SideControl.cpp
@@ -2,7 +2,7 @@
 
 SideControl::SideControl(int *busname)
 {
-    ModControl::file_i2c = &busname;
+    SideControl::file_i2c = &busname;
 }
 SideControl::setSerialNumber(char serialnumber[8]) {
     uint8_t buffer[9];
@@ -14,22 +14,22 @@ SideControl::setSerialNumber(char serialnumber[8]) {
             bytestosend++;
         }
     }   
-    if (ioctl(ModControl::file_i2c, I2C_SLAVE, SideControl::RIGHT_PANEL)) {
+    if (ioctl(SideControl::file_i2c, I2C_SLAVE, SideControl::RIGHT_PANEL)) {
         printf("Failed to acquire bus access and/or talk to slave.\n");
         return -1;
     }
-    if (write(ModControl::file_i2c, buffer, bytestosend) != bytestosend) {
+    if (write(SideControl::file_i2c, buffer, bytestosend) != bytestosend) {
         printf("Failed to write to the i2c bus.\n");
         return -1;
     }
     return 1;
 }
 SideControl::clearSerialNumber() {
-    if (ioctl(ModControl::file_i2c, I2C_SLAVE, SideControl::RIGHT_PANEL)) {
+    if (ioctl(SideControl::file_i2c, I2C_SLAVE, SideControl::RIGHT_PANEL)) {
         printf("Failed to acquire bus access and/or talk to slave.\n");
         return -1;
     }
-    if (write(ModControl::file_i2c, {0x20}, 1) != 1) {
+    if (write(SideControl::file_i2c, {0x20}, 1) != 1) {
         printf("Failed to write to the i2c bus.\n");
         return -1;
     }
@@ -42,44 +42,44 @@ SideControl::setIndicator(uint8_t panel, bool lit, char indicator[3]) {\
     for (int i = 0;i < 3; i++){
         buffer[i+2] = indicator[i];
     }
-    if (ioctl(ModControl::file_i2c, I2C_SLAVE, panel)) {
+    if (ioctl(SideControl::file_i2c, I2C_SLAVE, panel)) {
         printf("Failed to acquire bus access and/or talk to slave.\n");
         return -1;
     }
-    if (write(ModControl::file_i2c, buffer, 5) != 5) {
+    if (write(SideControl::file_i2c, buffer, 5) != 5) {
         printf("Failed to write to the i2c bus.\n");
         return -1;
     }
     return 1;
 }
 SideControl::clearAllIndicator(uint8_t panel) {
-    if (ioctl(ModControl::file_i2c, I2C_SLAVE, panel)) {
+    if (ioctl(SideControl::file_i2c, I2C_SLAVE, panel)) {
         printf("Failed to acquire bus access and/or talk to slave.\n");
         return -1;
     }
-    if (write(ModControl::file_i2c, {0x21}, 1) != 1) {
+    if (write(SideControl::file_i2c, {0x21}, 1) != 1) {
         printf("Failed to write to the i2c bus.\n");
         return -1;
     }
     return 1;
 }
 SideControl::setSideArt(uint8_t panel, uint8_t art]) {
-    if (ioctl(ModControl::file_i2c, I2C_SLAVE, panel)) {
+    if (ioctl(SideControl::file_i2c, I2C_SLAVE, panel)) {
         printf("Failed to acquire bus access and/or talk to slave.\n");
         return -1;
     }
-    if (write(ModControl::file_i2c, {0x12, art}, 2) != 2) {
+    if (write(SideControl::file_i2c, {0x12, art}, 2) != 2) {
         printf("Failed to write to the i2c bus.\n");
         return -1;
     }
     return 1;
 }
 SideControl::clearAllSideArt(uint8_t panel) {
-    if (ioctl(ModControl::file_i2c, I2C_SLAVE, panel)) {
+    if (ioctl(SideControl::file_i2c, I2C_SLAVE, panel)) {
         printf("Failed to acquire bus access and/or talk to slave.\n");
         return -1;
     }
-    if (write(ModControl::file_i2c, {0x22}, 1) != 1) {
+    if (write(SideControl::file_i2c, {0x22}, 1) != 1) {
         printf("Failed to write to the i2c bus.\n");
         return -1;
     }
