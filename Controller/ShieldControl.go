de6146080192f7894d747b73797cfbd5101c2038
Jackson Borneman <me@jaxb.net>
2021-11-21 12:10:11 -0500

    Added button checking

--- Controller/ShieldControl.go
+++ Controller/ShieldControl.go
@@ -1,54 +1,75 @@
-package Controller
+package controller
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/jax-b/go-i2c7Seg"
 	"github.com/stianeikeland/go-rpio/v4"
 )
-struct ShieldControl {
-	strikecount uint8
-	seg *i2c7Seg.SevenSegI2C
-	buzzerPin rpio.Pin
-	strike1Pin rpio.Pin
-	strike2Pin rpio.Pin
-	modInterruptPin rpio.Pin
-	randomStartPin rpio.Pin
+
+type ShieldControl struct {
+	strikecount         uint8
+	seg                 *i2c7Seg.SevenSegI2C
+	buzzerPin           rpio.Pin
+	strike1Pin          rpio.Pin
+	strike2Pin          rpio.Pin
+	modInterruptPin     rpio.Pin
+	mfbPin              rpio.Pin
+	mfbCallbackConsumer []chan uint16
+	m2cCallbackConsumer []chan bool
+	mfbEdge             rpio.Edge
+	stopBtnCheck        chan bool
 }
 
-func NewShieldControl(bus int, buzzerPinNum uint8, strike1PinNum uint8, strike2PinNum uint8, modInterruptPinNum uint8, randomStartPinNum uint8) (*ShieldControl, error) {
+func NewShieldControl(bus int, buzzerPinNum uint8, strike1PinNum uint8, strike2PinNum uint8, modInterruptPinNum uint8, mfbPinNum uint8) (*ShieldControl, error) {
 	i2c, err := i2c7Seg.NewSevenSegI2C(0x70, bus)
 	if err != nil {
 		return nil, err
 	}
-	err := rpio.Open()
+	err = rpio.Open()
 	if err != nil {
 		return nil, err
 	}
 	sc := &ShieldControl{
-		seg: i2c,
-		strikecount: 0,
-		buzzerPin : rpio.Pin(buzzerPinNum),
-		strike1Pin : rpio.Pin(strike1PinNum),
-		strike2Pin : rpio.Pin(strike2PinNum),
-		modInterruptPin : rpio.Pin(modInterruptPinNum),
-		randomStartPin : rpio.Pin(randomStartPinNum),
+		seg:             i2c,
+		strikecount:     0,
+		buzzerPin:       rpio.Pin(buzzerPinNum),
+		strike1Pin:      rpio.Pin(strike1PinNum),
+		strike2Pin:      rpio.Pin(strike2PinNum),
+		modInterruptPin: rpio.Pin(modInterruptPinNum),
+		mfbPin:          rpio.Pin(mfbPinNum),
 	}
 	// Configure pins
 	sc.buzzerPin.Mode(rpio.Pwm)
 	sc.strike1Pin.Output()
 	sc.strike2Pin.Output()
-	sc.randomStartPin.Input()
-	sc.randomStartPin.PullUp()
-	sc.randomStartPin.Detect(rpio.RiseEdge)
+	sc.mfbPin.Input()
+	sc.mfbPin.PullUp()
+	sc.mfbPin.Detect(rpio.RiseEdge)
+	sc.mfbEdge = rpio.RiseEdge
 	sc.modInterruptPin.Input()
 	sc.modInterruptPin.PullUp()
 	sc.modInterruptPin.Detect(rpio.FallEdge)
 
+	go func() {
+		stop := false
+		for !stop {
+			sc.btnCheck()
+			select {
+			case stop = <-sc.stopBtnCheck:
+			default:
+			}
+		}
+	}()
 	return sc, nil
 }
 func (self *ShieldControl) Close() {
 	self.seg.Close()
 	rpio.Close()
+	self.stopBtnCheck <- true
 }
+
 // Adds a strike to the display system and plays a sound
 func (self *ShieldControl) AddStrike() {
 	self.strikecount++
@@ -65,27 +86,30 @@ func (self *ShieldControl) AddStrike() {
 		self.strike2Pin.High()
 	}
 }
+
 // Resets the strike to zero
 func (self *ShieldControl) ResetStrike() {
 	self.strikecount = 0
 	self.strike1Pin.Low()
 	self.strike2Pin.Low()
 }
+
 // plays the sound of a strike
 func (self *ShieldControl) buzzStrikeSound() {
-	buzzerPin.Freq(64000)
-	buzzerPin.DutyCycle(16, 32)
+	self.buzzerPin.Freq(64000)
+	self.buzzerPin.DutyCycle(16, 32)
 	time.Sleep(time.Millisecond * 500)
-	buzzerPin.DutyCycle(0, 32)
+	self.buzzerPin.DutyCycle(0, 32)
 }
+
 // TimeClockSignal
 func (self *ShieldControl) TimeSigBeep(multiplier float32) {
-	buzzerPin.Freq(600)
-	buzzerPin.DutyCycle(16, 32)
-	time.Sleep(time.Millisecond * (350 * multiplier))
-	buzzerPin.Freq(60)
-	time.Sleep(time.Millisecond * (250 * multiplier)
-	buzzerPin.DutyCycle(0, 32)
+	self.buzzerPin.Freq(600)
+	self.buzzerPin.DutyCycle(16, 32)
+	time.Sleep(time.Millisecond * time.Duration(350*multiplier))
+	self.buzzerPin.Freq(60)
+	time.Sleep(time.Millisecond * time.Duration(250*multiplier))
+	self.buzzerPin.DutyCycle(0, 32)
 }
 func (self *ShieldControl) WriteIdle() {
 	self.seg.Clear()
@@ -126,4 +150,53 @@ func (self *ShieldControl) WriteTime(timemilis uint32) {
 func (self *ShieldControl) ClearDisplay() {
 	self.seg.Clear()
 	self.seg.WriteDisplay()
+}
+func (self *ShieldControl) RegisterM2CConsumer() chan bool {
+	c := make(chan bool)
+	self.m2cCallbackConsumer = append(self.m2cCallbackConsumer, c)
+
+	return c
+}
+func (self *ShieldControl) RegisterMFBConsumer() chan uint16 {
+	c := make(chan uint16)
+	self.mfbCallbackConsumer = append(self.mfbCallbackConsumer, c)
+
+	return c
+}
+func (self *ShieldControl) btnCheck() {
+	// if we have a signal from a downstream controller signal all consumers (nonblocking)
+	if self.modInterruptPin.EdgeDetected() {
+		for _, c := range self.m2cCallbackConsumer {
+			go func(c chan bool) {
+				c <- true
+			}(c)
+		}
+	}
+	// if the button state is changed, and we are looking for a press
+	if self.mfbPin.EdgeDetected() && self.mfbEdge == rpio.RiseEdge {
+		// Span a new concurent to wait for release
+		go func() {
+			// Set up the edge detection
+			self.mfbEdge = rpio.FallEdge
+			self.mfbPin.Detect(rpio.FallEdge)
+			// Record time of press
+			mfbPush := time.Now()
+			// Wait for release
+			for !self.mfbPin.EdgeDetected() {
+			}
+			// Record time of release and compute difference
+			mfbRelease := time.Now()
+			mfbPushTime := uint16(mfbRelease.Sub(mfbPush).Milliseconds())
+			// Signal all consumers for how long the button was pressed
+			for _, c := range self.mfbCallbackConsumer {
+				// Non blocking channel update
+				go func(c chan uint16) {
+					c <- mfbPushTime
+				}(c)
+			}
+			// Reset edge detection
+			self.mfbEdge = rpio.RiseEdge
+			self.mfbPin.Detect(rpio.RiseEdge)
+		}()
+	}
 }
