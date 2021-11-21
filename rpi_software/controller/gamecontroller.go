8fb5813b2d53328a2cf5a9791873c6c04ffe62a1
Jackson Borneman <me@jaxb.net>
2021-11-21 18:38:05 -0500

    Added cmds to gamectrl and ipc

--- rpi_software/controller/gamecontroller.go
+++ rpi_software/controller/gamecontroller.go
@@ -1,7 +1,7 @@
 package controller
 
 import (
-	"github.com/jax-b/ModulerKTNE/controller"
+	"github.com/jax-b/ModulerKTNE/rpi_software/controller"
 )
 
 type GameController struct {
@@ -10,8 +10,74 @@ type GameController struct {
 		mctrl   *controller.ModControl
 		present bool
 	}
-	rpishield *controller.ShieldControl
-	ipc       *controller.InterProcessCom
-	mnetc     *controller.MultiCastCountdown
-	cfg       *controller.Config
+	rpishield  *controller.ShieldControl
+	ipc        *controller.InterProcessCom
+	mnetc      *controller.MultiCastCountdown
+	cfg        *controller.Config
+	gameTime   uint16
+	gameStopCh chan bool
+	numStrike  uint8
+}
+
+// Gets the current game time
+func (self *GameController) GetTime() uint16 {
+	return &self.gameTime
+}
+
+// Sets the game time to the given time
+func (self *GameController) SetTime(time uint16) {
+	for mod := range modules {
+		if mod.present {
+			mod.mctrl.SetTime(time)
+		}
+	}
+	self.gameTime = time
+}
+func (self *GameController) StartGame() error {
+	// for all the modules that are present, start the game
+	self.scanModules()
+	for mod := range modules {
+		if mod.present {
+			mod.mctrl.StartGame()
+		}
+	}
+	self.gameStopCh = make(chan bool)
+	go self.timer(self.gameStopCh)
+	return nil
+}
+func (self *GameController) StopGame() error {
+	// for all the modules that are present, stop the game
+	for mod := range modules {
+		if mod.present {
+			mod.mctrl.StopGame()
+		}
+	}
+
+	self.gameStopCh <- true
+	return nil
+}
+func (self *GameController) timer(StopCh chan bool) {
+	ticker := time.NewTicker(time.Millisecond)
+	for {
+		select {
+		case <-StopCh:
+			return
+		case <-ticker:
+			self.gameTime--
+		}
+		if self.gameTime == 0 {
+			self.timerRunOut()
+			return
+		}
+	}
+}
+func (self *GameController) scanModules() {
+	for mod := range modules {
+		mod.present = mod.TestIfPresent()
+	}
+}
+func (self *GameController) timerRunOut() {
+	self.StopGame()
+	self.mnetc.SendTimeStrike(0, self.numStrike, true, false)
+	self.ipc.TimerRunOut()
 }
--- rpi_software/controller/interProcessCom.go
+++ rpi_software/controller/interProcessCom.go
@@ -1,28 +1,94 @@
 package controller
 
 import (
-	"rpc"
+	"github.com/james-barrow/golang-ipc"
 
-	"github.com/jax-b/ModulerKTNE/Controller"
+	"github.com/jax-b/ModulerKTNE/rpi_software/controller"
+	"go.uber.org/zap"
 )
 
 type InterProcessCom struct {
-	rpc   *rpc.RPC
-	close chan bool
-	game  *Controller.GameController
+	ipc     *ipc.Server
+	closech chan bool
+	game    *Controller.GameController
+	logger  *zap.Logger
 }
 
 // Creates a new interprocess communicator
-func NewIPC(gamectrl *Controller.GameController) *InterProcessCom {
-	return &InterProcessCom{
-		rpc:   rpc.NewRPC(),
-		close: make(chan bool),
-		game:  gamectrl,
+func NewIPC(gamectrl *Controller.GameController) (*InterProcessCom, error) {
+	scon := &ipc.ServerConfig{
+		Timeout: time.Millisecond * 50,
 	}
+	sv, err := ipc.StartServer("ktne-ipc", scon)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	ipc := &InterProcessCom{
+		ipc:     sv,
+		closech: make(chan bool),
+		game:    gamectrl,
+	}
+	return ipc, nil
 }
 
 // Safely closes the interprocess communicator
-func (ipc *InterProcessCom) Close() error {
-	ipc.close <- true
+func (self *InterProcessCom) Close() error {
+	self.closech <- true
+	self.ipc.Close()
 	return nil
+}
+
+func (self *InterProcessCom) commandTree() {
+	if self.ipc.StatusCode() = 3{
+		message, err := self.ipc.Read()
+		if err != nil {
+			log.Println(err)
+		}
+		message = strings.Split(message)
+		switch message[0] {
+			case "mktne.start_game":
+				err := self.game.StartGame()
+				if err == nil {
+					self.ipc.Write([]byte("mktne.start_game.ok"))
+				} else {
+					self.logger.error("Failed to Start Game:", err)
+					self.ipc.Write([]byte("mktne.start_game.error"))
+				}
+				break
+			case "mktne.stop_game":
+				err := self.game.StopGame()
+				if err == nil {
+					self.ipc.Write([]byte("mktne.stop_game.ok"))
+				} else {
+					self.logger.error("Failed to Stop ipc:", err)
+					self.ipc.Write([]byte("mktne.stop_game.error"))
+				}
+				break
+			case "mktne.get_time":
+				gametime := self.game.get_time()
+				buffer := []byte("mktne.gametime:")
+				buffer = buffer.append(buffer,[]byte(strconv.Itoa(gametime)))
+				self.ipc.Write(buffer)
+				break
+			case "mktne.set_time":
+				gametime, err := strconv.ParseInt(message[1],10,16)
+				if err != nil {
+					self.logger.error("Failed to convert time:", err)
+					self.ipc.Write([]byte("mktne.set_time.error"))
+				}
+				err = self.game.set_time(gametime)
+				if err == nil {
+					self.ipc.Write([]byte("mktne.set_time.ok"))
+				} else {
+					self.logger.error("Failed to set time:", err)
+					self.ipc.Write([]byte("mktne.set_time.error"))
+				}
+				break
+		}
+	}
+}
+
+func (self *InterProcessCom) timerRunOut() {
+	self.ipc.Write([]byte("mktne.timer_runout"))
 }
