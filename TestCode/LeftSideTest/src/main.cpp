81767a959a85a3b89ea757075c42736446d283ac
Jackson Borneman <me@jaxb.net>
2022-06-29 20:01:54 -0400

    Added example code

--- TestCode/LeftSideTest/src/main.cpp
+++ TestCode/LeftSideTest/src/main.cpp
@@ -1,10 +1,9 @@
 // #include <Ardiuno.h>
 // #include <SPI.h>
-
+#include <Adafruit_GFX.h>
 #include <GxEPD2_3C.h> 
-
-
-
+#include "bitmaps/Bitmaps104x212.h"
+#include "bitmaps/Bitmaps3c104x212.h"
 #define EPD_RESET_PIN 5
 #define EPD_DC_PIN 4
 #define EPD_MOSI_PIN 3
@@ -21,13 +20,67 @@
 #define DSP6_CS_PIN 14
 #define DSP6_LED_PIN 15
 
+#define DSP_Master true
 
-GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> IndicatorDSP1(GxEPD2_290_C90c(/*CS=*/DSP1_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/EPD_BUS_PIN)); //Indicator
-GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> IndicatorDSP2(GxEPD2_290_C90c(/*CS=*/DSP2_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/EPD_BUS_PIN)); //Ind
-// GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> ArtDSP1(GxEPD2_290_C90c(/*CS=*/DSP3_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/DSP3_BUS_PIN));
-// GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> ArtDSP2(GxEPD2_290_C90c(/*CS=*/DSP4_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/DSP4_BUS_PIN));
-// GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> IndicatorDSP3(GxEPD2_290_C90c(/*CS=*/DSP5_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/DSP5_BUS_PIN));
-// GxEPD2_3C<GxEPD2_290_C90c, GxEPD2_290_C90c::HEIGHT> IndicatorDSP4(GxEPD2_290_C90c(/*CS=*/DSP6_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/DSP6_BUS_PIN));
+GxEPD2_3C<GxEPD2_290_C90c, 1> IndicatorDSP1(GxEPD2_290_C90c(/*CS=*/DSP1_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/EPD_BUS_PIN)); //Indicator
+GxEPD2_3C<GxEPD2_290_C90c, 1> IndicatorDSP2(GxEPD2_290_C90c(/*CS=*/DSP2_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/EPD_BUS_PIN)); //Ind
+GxEPD2_3C<GxEPD2_290_C90c, 1> ArtDSP1(GxEPD2_290_C90c(/*CS=*/DSP3_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/EPD_BUS_PIN));
+GxEPD2_3C<GxEPD2_290_C90c, 1> ArtDSP2(GxEPD2_290_C90c(/*CS=*/DSP4_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/EPD_BUS_PIN));
+GxEPD2_3C<GxEPD2_290_C90c, 1> IndicatorDSP3(GxEPD2_290_C90c(/*CS=*/DSP5_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/EPD_BUS_PIN));
+#ifdef DSP_Master
+GxEPD2_3C<GxEPD2_290_C90c, 1> IndicatorDSP4(GxEPD2_290_C90c(/*CS=*/DSP6_CS_PIN, /*DC=*/EPD_DC_PIN, -1, /*BUSY=*/EPD_BUS_PIN));
+#endif
+
+GFXcanvas1 canvas(250, 122); 
+
+#define MAX_HEIGHT_3C(EPD) (120)
+#define pgm_spisettings SPISettings(4000000, MSBFIRST, SPI_MODE0)
+void dspinit() {
+    pinMode(EPD_RESET_PIN, OUTPUT);
+    pinMode(EPD_DC_PIN, OUTPUT);
+    pinMode(DSP1_CS_PIN, OUTPUT);
+    pinMode(DSP2_CS_PIN, OUTPUT);
+    pinMode(DSP3_CS_PIN, OUTPUT);
+    pinMode(DSP4_CS_PIN, OUTPUT);
+    pinMode(DSP5_CS_PIN, OUTPUT);
+    pinMode(DSP6_CS_PIN, OUTPUT);
+    pinMode(EPD_BUS_PIN, INPUT);
+
+    digitalWrite(DSP1_CS_PIN, false);
+    digitalWrite(DSP2_CS_PIN, false);
+    digitalWrite(DSP3_CS_PIN, false);
+    digitalWrite(DSP4_CS_PIN, false);
+    digitalWrite(DSP5_CS_PIN, false);
+    digitalWrite(DSP6_CS_PIN, false);
+    
+
+    Serial.println("DSP RESET");
+    digitalWrite(EPD_RESET_PIN, true);
+    delay(100);
+    digitalWrite(EPD_RESET_PIN, false);
+    delay(100);
+    digitalWrite(EPD_RESET_PIN, true);
+
+    Serial.println("DSP INIT");
+
+    IndicatorDSP1.init();
+    IndicatorDSP1.writeScreenBuffer(0xFF);
+    IndicatorDSP2.writeScreenBuffer(0xFF);
+    ArtDSP1.writeScreenBuffer(0xFF);
+    ArtDSP2.writeScreenBuffer(0xFF);
+    IndicatorDSP3.writeScreenBuffer(0xFF);
+    #ifdef DSP_Master
+    IndicatorDSP4.writeScreenBuffer(0xFF);
+    #endif
+    // Release Displays
+    digitalWrite(DSP1_CS_PIN, true);
+    digitalWrite(DSP2_CS_PIN, true);
+    digitalWrite(DSP3_CS_PIN, true);
+    digitalWrite(DSP4_CS_PIN, true);
+    digitalWrite(DSP5_CS_PIN, true);
+    digitalWrite(DSP6_CS_PIN, true);
+    delay(10);
+}
 
 void setup(){
     // MbedSPI SPI(0, EPD_MOSI_PIN, EPD_SCK_PIN);
@@ -42,40 +95,21 @@ void setup(){
     delay(1000);
     Serial.println("Start");
 
-    pinMode(EPD_RESET_PIN, OUTPUT);
+    
+    pinMode(DSP6_LED_PIN, OUTPUT);
     pinMode(DSP1_LED_PIN, OUTPUT);
     pinMode(DSP2_LED_PIN, OUTPUT);
     pinMode(DSP5_LED_PIN, OUTPUT);
     pinMode(DSP6_LED_PIN, OUTPUT);
 
-    digitalWrite(EPD_RESET_PIN, true);
-    digitalWrite(DSP1_LED_PIN, false);
-    digitalWrite(DSP2_LED_PIN, false);
-    digitalWrite(DSP5_LED_PIN, false);
-    digitalWrite(DSP6_LED_PIN, false);
 
-    Serial.println("DSP RESET");
-    digitalWrite(EPD_RESET_PIN, true);
-    delay(50);
-    digitalWrite(EPD_RESET_PIN, false);
-    delay(50);
-    digitalWrite(EPD_RESET_PIN, true);
+
+    
 
     Serial.println("DSP Init");
-    IndicatorDSP1.init();
-    IndicatorDSP2.init();
-    // ArtDSP1.init();
-    // ArtDSP2.init();
-    // IndicatorDSP3.init();
-    // IndicatorDSP4.init();
+    dspinit();
 
-    digitalWrite(DSP1_CS_PIN, true);
-    digitalWrite(DSP2_CS_PIN, true);
-    digitalWrite(DSP3_CS_PIN, true);
-    digitalWrite(DSP4_CS_PIN, true);
-    digitalWrite(DSP5_CS_PIN, true);
-    digitalWrite(DSP6_CS_PIN, true);
-
+    
     Serial.println("LED Light Test");
     digitalWrite(DSP1_LED_PIN, true);
     delay(500);
@@ -93,30 +127,26 @@ void setup(){
     delay(500);
     digitalWrite(DSP6_LED_PIN, false);
     delay(100);
-    
+
     Serial.println("DSP Draw");
 
-    IndicatorDSP1.firstPage();
-    IndicatorDSP1.setRotation(3);
-    IndicatorDSP1.fillRect(0,0,250,122,GxEPD_WHITE);
-    IndicatorDSP1.setTextColor(GxEPD_BLACK); 
-    IndicatorDSP1.setTextSize(2);
-    IndicatorDSP1.println("Hello World!");
-    IndicatorDSP1.setTextColor(GxEPD_RED);
-    IndicatorDSP1.println("Want pi?");
-    IndicatorDSP1.displayWindow(0,0,250,122);
+    IndicatorDSP1.writeImage(WS_Bitmap104x212, 0, 0, 104, 212, 0,0,1);
+    ArtDSP1.writeImage(WS_Bitmap3c104x212_black, WS_Bitmap3c104x212_red, 0, 0, 104, 212, 0,0,1);
 
-    IndicatorDSP2.firstPage();
-    IndicatorDSP2.setRotation(3);
-    IndicatorDSP2.fillRect(0,0,250,122,GxEPD_WHITE);
-    IndicatorDSP2.setCursor(10,10);
-    IndicatorDSP2.setTextSize(10);
-    IndicatorDSP2.setTextColor(GxEPD_BLACK); 
-    IndicatorDSP2.print("2");
-    IndicatorDSP2.setTextColor(GxEPD_RED);
-    IndicatorDSP2.println("2");
-    IndicatorDSP2.displayWindow(0,0,250,122);
 
+    digitalWrite(DSP1_CS_PIN, false);
+    digitalWrite(DSP2_CS_PIN, false);
+    digitalWrite(DSP3_CS_PIN, false);
+    digitalWrite(DSP4_CS_PIN, false);
+    digitalWrite(DSP5_CS_PIN, false); 
+    digitalWrite(DSP6_CS_PIN, false);
+    IndicatorDSP1.refresh(false);
+    digitalWrite(DSP1_CS_PIN, true);
+    digitalWrite(DSP2_CS_PIN, true);
+    digitalWrite(DSP3_CS_PIN, true);
+    digitalWrite(DSP4_CS_PIN, true);
+    digitalWrite(DSP5_CS_PIN, true);
+    digitalWrite(DSP6_CS_PIN, true);
 }
 
 void loop(){
